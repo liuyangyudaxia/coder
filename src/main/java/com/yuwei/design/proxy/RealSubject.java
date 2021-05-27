8585b03b1ccae741f7821fcc9272fdf197bf87d2
yuwei <974278494@qq.com>
2021-05-28 00:20:03 +0800

    设计模式第三天

---
+++ src/main/java/com/yuwei/design/proxy/RealSubject.java
@@ -0,0 +1,13 @@
+package com.yuwei.design.proxy;
+
+/**
+ * 真实主题
+ * @author yuwei
+ * @date 2021/5/27 23:09
+ */
+public class RealSubject implements Subject {
+    @Override
+    public void request() {
+        System.out.println("访问真实主题方法...");
+    }
+}
---
+++ src/main/java/com/yuwei/design/proxy/Subject.java
@@ -0,0 +1,10 @@
+package com.yuwei.design.proxy;
+
+/**
+ * 抽象主题
+ * @author yuwei
+ * @date 2021/5/27 23:06
+ */
+interface Subject {
+    void request();
+}

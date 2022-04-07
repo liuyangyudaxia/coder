b38fd3c330f2d0f0beb70e5bd946489330a6854e
yuwei <yuwei_it@163.com>
2022-04-08 00:42:19 +0800

    sort

--- algorithm/src/sort/BubbleSort.java
+++ algorithm/src/sort/BubbleSort.java
@@ -1,7 +1,6 @@
 package sort;
 
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * @author yuwei
@@ -13,10 +12,13 @@ public class BubbleSort {
             array[i] = (int) (Math.random() * 100);
         }
         System.out.println("排序前：" + Arrays.toString(array));
-        sort1(array);
+        sort3(array);
         System.out.println("排序后：" + Arrays.toString(array));
     }
 
+    /**
+     * 冒泡排序基础
+     */
     public static void sort1(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
@@ -26,6 +28,58 @@ public class BubbleSort {
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
+            }
+        }
+    }
+
+    /**
+     * 冒泡排序优化
+     */
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            //有序标记，每一轮初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                int temp = 0;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    //存在交换，无序
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 冒泡排序边界优化
+     */
+    public static void sort3(int[] array) {
+        //最后一次交换位置
+        int lastExchangeIndex = 0;
+        //无序边界
+        int sortBorder = array.length - 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            //有序标记，每一轮初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    //存在交换，无序
+                    isSorted = false;
+                    //最后一次交换元素的位置
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            if (isSorted) {
+                break;
             }
         }
     }

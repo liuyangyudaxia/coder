ee750794988255820e2d766b0b29e4e2f34a7c7c
yuwei <yuwei_it@163.com>
2022-04-07 23:53:34 +0800

    sort

---
+++ algorithm/src/sort/BubbleSort.java
@@ -0,0 +1,32 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author yuwei
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println("排序前：" + Arrays.toString(array));
+        sort1(array);
+        System.out.println("排序后：" + Arrays.toString(array));
+    }
+
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                int temp = 0;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
